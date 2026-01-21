04bbb7f4366d0d8c09dc15992a78ffc9829798a8
jaydevvaghela943 <jaydevvaghela943@gmail.com>
2026-01-21 18:29:15 +0530

    Added Admin side changes

--- core/vege/models.py
+++ core/vege/models.py
@@ -1,7 +1,7 @@
-from django.db import models
+from django.db import models # type: ignore
 
 #authentication
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User # type: ignore
                                 
 
 # Create your models here.
@@ -10,3 +10,37 @@ class Receipe(models.Model):
     receipe_name = models.CharField(max_length=200)
     receipe_description = models.TextField()
     receipe_image = models.ImageField(upload_to='receipe')
+    receipe_view_count = models.IntegerField(default=1)
+
+# department model
+class Department(models.Model):
+    department = models.CharField(max_length=100)
+
+    def __str__(self)->str:
+        return self.department
+    
+    class Meta:
+        ordering = ['department']
+
+#Student model
+class StudentID(models.Model):
+    student_id = models.CharField(max_length=100)
+
+    def __str__(self)->str:
+        return self.student_id
+
+class Student(models.Model):
+    department = models.ForeignKey(Department,related_name="depart", on_delete=models.CASCADE)
+    student_id = models.OneToOneField(StudentID,related_name="studentid", on_delete=models.CASCADE)
+    student_name = models.CharField(max_length=100)
+    student_email = models.EmailField(unique=True)
+    student_age = models.IntegerField(default=18)
+    student_address =  models.TextField()
+
+    def __str__(self)->str:
+        return self.student_name
+    
+    class Meta:
+        ordering = ['student_name']
+        verbose_name = "student"
+    
---
+++ core/vege/seed.py
@@ -0,0 +1,31 @@
+from faker import Faker
+import random
+from .models import *
+
+fake = Faker()
+
+def seed_db(n=10)->None:
+    try:
+        for i in range(0,n):
+            department_objs = Department.objects.all()
+            department_index= random.randint(0,len(department_objs)-1)
+            department      = department_objs[department_index]
+
+            department = department
+            student_id = f'STU-0{random.randint(100,999)}'
+            student_name = fake.name()
+            student_email = fake.email()
+            student_age = random.randint(20,35)
+            student_address = fake.address()
+
+            student_id_obj = StudentID.objects.create(student_id=student_id)
+            student_obj = Student.objects.create(
+                department 		=department,
+                student_id 		=student_id_obj,
+                student_name 	=student_name,
+                student_email 	=student_email,
+                student_age 	=student_age,
+                student_address =student_address
+            )
+    except Exception as e:
+        print("Err===>",e) 
